5c9343dbc2a06278de71474ef170bd2e8519bd2a
t1993617 <t1993617@gmail.com>
2019-10-27 10:40:33 +0800

    bug test

--- Test1/Controllers/HomeController.cs
+++ Test1/Controllers/HomeController.cs
@@ -3,11 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Data.SqlClient;
+
+
 
 namespace Test1.Controllers
 {
     public class HomeController : Controller
     {
+   
+
         public class Student
         {
             public string id { get; set; }
@@ -41,12 +46,120 @@ namespace Test1.Controllers
             list.Add(new Student("3", "小英", 60));
             list.Add(new Student("4", "小李", 50));
             list.Add(new Student("5", "小張", 90));
+            list.Add(new Student("!!!!!", "~~~~~~~", 8));
+
+            //test範圍=================================================
+
+            /*
+             //   Data Source = LAPTOP - 2SE654DN\SQLEXPRESS; Initial Catalog = Northwind; Integrated Security = True
+
+            SqlConnection sqlConnection = new SqlConnection(@"Server =LAPTOP - 2SE654DN\SQLEXPRESS; Database =  Northwind; Trusted_Connection = SSPI");
+            SqlCommand sqlCommand = new SqlCommand("SELECT * FROM Customers");
+            sqlCommand.Connection = sqlConnection;
+            sqlConnection.Open();
+
+            SqlDataReader reader = sqlCommand.ExecuteReader();
+            if (reader.HasRows)
+            {
+                while (reader.Read())
+                {
+                    Card card = new Card
+                    {
+                        ID = reader.GetInt32(reader.GetOrdinal("id")),
+                        Char_name = reader.GetString(reader.GetOrdinal("char_name")),
+                        Card_name = reader.GetString(reader.GetOrdinal("card_name")),
+                        Card_level = reader.GetString(reader.GetOrdinal("card_level")),
+                    
+                    };
+                   
+                }
+            }
+    */
+            //====================================================
 
             ViewBag.Date = date;
             ViewBag.Student = data;
             ViewBag.List = list;
             return View();
+ 
+        
+        
         }
+
+
+
+
+
+
+
+
+
+        public class Card
+        {
+            public int ID { get; set; }
+            public string Char_name { get; set; }
+            public string Card_name { get; set; }
+            public string Card_level { get; set; }
+        }
+
+
+
+        public List<Card> GetCards()
+        {
+            ViewBag.Date = "cccccccccccc";
+            List<Card> cards = new List<Card>();
+            SqlConnection sqlConnection = new SqlConnection(@"Data Source = LAPTOP - 2SE654DN\SQLEXPRESS; Initial Catalog = Northwind; Integrated Security = True");
+            SqlCommand sqlCommand = new SqlCommand("SELECT * FROM Customers");
+            sqlCommand.Connection = sqlConnection;
+            sqlConnection.Open();
+
+            SqlDataReader reader = sqlCommand.ExecuteReader();
+            if (reader.HasRows)
+            {
+                while (reader.Read())
+                {
+                    Card card = new Card
+                    {
+                        ID = reader.GetInt32(reader.GetOrdinal("id")),
+                        Char_name = reader.GetString(reader.GetOrdinal("char_name")),
+                        Card_name = reader.GetString(reader.GetOrdinal("card_name")),
+                        Card_level = reader.GetString(reader.GetOrdinal("card_level")),
+                    };
+                    cards.Add(card);
+                }
+            }
+            else
+            {
+                Console.WriteLine("資料庫為空！");
+            }
+            sqlConnection.Close();
+            return cards;
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
